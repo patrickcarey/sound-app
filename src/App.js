e59d60d7cd5e9122b3a41cfffb88f1cc08e90fa4
Jack Reid <emailjackreid@gmail.com>
2018-10-24 21:57:21 +0100

    Make a start on a grid sequencer
    - Create a state object in the App component
    - Create a generator for the initial grid state
    - Render the grid by mapping through the state object
    - Pass down a toggleButton function that modifies the top state
    - Some styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,37 @@ import Slider from 'rc-slider';
 import './App.css';
 import 'rc-slider/assets/index.css';
 
+function generateInitialGridState({ width = 1, height = 1}) {
+  let id = 0;
+  const outerMatrix = [];
+
+  for (let i = 0; i < width; i++) {
+    const innerMatrix = [];
+    for (let j = 0; j < height; j++) {
+      innerMatrix.push({ id, state: 'active' });
+      id++;
+    }
+    outerMatrix.push(innerMatrix);
+  }
+
+  return outerMatrix;
+}
+
 class App extends Component {
+  state = {
+    gridLayout: generateInitialGridState({ width: 8, height: 3 })
+  }
+
+  toggleButton = buttonNumber => this.setState(previous => {
+    const gridLength = previous.gridLayout.length;
+    const gridHeight = previous.gridLayout[0].length;
+    const buttonColumn = Math.floor(buttonNumber / gridHeight);
+    const buttonRow = buttonNumber % gridHeight;
+    const previousButtonState = previous.gridLayout[buttonColumn][buttonRow].state;
+    previous.gridLayout[buttonColumn][buttonRow].state = previousButtonState === 'active' ? 'inactive' : 'active'; 
+    return previous;
+  });
+
   render() {
     return (
       <div className="App">
@@ -17,9 +47,10 @@ class App extends Component {
         </div>
         
         <div className="seq">
-
-          <Grid className="grid" />
-          <Grid className="grid" />
+          <Grid
+            className="grid"
+            toggleButton={this.toggleButton}
+            gridLayout={this.state.gridLayout} />
         </div>
 
       </div>
--- src/button.js
+++ src/button.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 class Button extends Component {
   render() {
+    const { id, state, toggleButton } = this.props;
+
     return (
-      <div className="Button">
+      <div
+        onClick={() => toggleButton(id)}
+        className={`Button ${ state === 'active' && 'Button--active'}`}>
+        {id + 1}
       </div>
     );
   }
